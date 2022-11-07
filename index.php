7e8768ae6e0ce056b69b37c435902be6f4bbd377
Raphaël MANGIN <raphael.mangin6@etu.univ-lorraine.fr>
2022-11-07 11:15:22 +0100

    ajout de l'index

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
 <?php
+
+
+declare(strict_types=1);
+
+use iutnc\netvod\dispatch\Dispatcher;
+
+require_once 'vendor/autoload.php';
+
+session_start();
+
+$action = isset($_GET['action']) ? $_GET['action'] : null;
+$dispatcher = new Dispatcher($action);
+$dispatcher->run();
