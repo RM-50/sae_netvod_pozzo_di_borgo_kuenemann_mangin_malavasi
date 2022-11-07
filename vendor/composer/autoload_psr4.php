d373279503fef25abb1972f407f4389c1034f206
Raphaël MANGIN <raphael.mangin6@etu.univ-lorraine.fr>
2022-11-07 11:17:14 +0100

    modifictation autoloader

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'iutnc\\action\\' => array($baseDir . '/src/classes'),
+    'iutnc\\netvod\\' => array($baseDir . '/src/classes'),
 );
