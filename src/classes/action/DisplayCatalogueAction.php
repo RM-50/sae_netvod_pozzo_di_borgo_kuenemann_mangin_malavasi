ce83dd1b8c0be86bb1b2d5cc6babd46f087eddef
Nicolas Kuenemann <nicolas.kuenemann3@etu.univ-lorraine.fr>
2022-11-09 23:53:40 +0100

    Finalisation de RechMotCle.php qui recherche par mot cle une serie dans catalogue

--- src/classes/action/DisplayCatalogueAction.php
+++ src/classes/action/DisplayCatalogueAction.php
@@ -5,6 +5,7 @@ namespace iutnc\netvod\action;
 use iutnc\netvod\db\ConnectionFactory;
 use iutnc\netvod\render\Renderer;
 use iutnc\netvod\render\SerieRenderer;
+use iutnc\netvod\tri\RechMotCle;
 use iutnc\netvod\video\Episode;
 use iutnc\netvod\video\Serie;
 use PDOException;
@@ -13,50 +14,122 @@ class  DisplayCatalogueAction extends Action
 {
     public function execute(): string
     {
-        $html = '';
-        if (isset($_SESSION['user_connected']))
-        {
-            $html .= "  <div><h1> <a> Notre catalogue : </a></h1></div>";
-            $sqlSerie = "SELECT * FROM serie";
-            $sqlLstEps = "SELECT titre, file FROM episode where serie_id = ?";
+        $html = <<<END
+                <form id='rechercheMotCle' method='POST' action='?action=display-catalogue'>
+                    <label for="cle">Mot clé de recherche :</label>
+                    <input type="text" name="cle">
+                    <button type="submit">Rechercher !</button>
+                </form>
+                END;
+        if ($this->http_method === "POST") {
+            if (isset($_POST["cle"])) {
+                $idSeries=RechMotCle::triParMotCle($_POST["cle"]);
+                $html .= $this->afficherCatalogue($idSeries);
+            }
+        }else {
+            $html .= $this->afficherCatalogue();
+        }
 
-            try{
-                $db = ConnectionFactory::makeConnection();
-                $stmt_serie = $db->prepare($sqlSerie);
-                $stmt_serie->execute();
+        return $html;
+    }
 
-                while ($row_serie = $stmt_serie->fetch(\PDO::FETCH_ASSOC)) {
-                    $stmt = $db->prepare($sqlLstEps);
-                    $stmt->bindParam(1,$row_serie['id']);
-                    $stmt->execute();
-                    $listeEpisode = [];
-                    while($row = $stmt->fetch(\PDO::FETCH_ASSOC)){
-                        $listeEpisode [] = new Episode($row['titre'], $row['file']);
+    public function afficherCatalogue(array $idSeries=[]):string
+    {
+        $html = "<div><h1> <a> Notre catalogue : </a></h1></div> <br>";
+        if ($idSeries == []) {
+            if (isset($_SESSION['user_connected']))
+            {
+                try{
+                    $stmt_serie = $this->requestSerie();
+                    while ($row_serie = $stmt_serie->fetch(\PDO::FETCH_ASSOC)) {
+                        $id = 0;
+                        $html .= $this->getHtml($row_serie, $html,$id);
                     }
-                    $serie = new Serie($row_serie['titre'], $listeEpisode);
-                    $renderer = new SerieRenderer($serie);
-
-                    $user = unserialize($_SESSION["user_connected"]);
-                    $pref = $user->pref;
-                    $favoris = "";
-                    if ($pref->isPref($row_serie["id"])) {
-                        $favoris = "&favoris=2";
-                    }
-                    $html .= " 
-                        
-                        <a href='?action=display-serie&id={$row_serie['id']}$favoris'>
-                         {$renderer->render(Renderer::COMPACT)} 
-                        </a>     
-                      "
-                    ;
-
+                }catch (PDOException $e){
+                    echo $e->getMessage();
                 }
+
+            }
+
+        }else {
+            $stmt_serie = $this->requestSerie();
+            while ($row_serie = $stmt_serie->fetch(\PDO::FETCH_ASSOC)) {
+                $id = $row_serie['id'];
+                if (isset($idSeries[$id])) {
+                    $html .= $this->getHtml($row_serie, $html,$id);
+                }
+            }
+
+            }
+        return $html;
+        }
+
+        public function requestSerie():\PDOStatement
+        {
+            $stmt_serie = null;
+            try {
+
+            $sqlSerie = "SELECT * FROM serie";
+
+            $db = ConnectionFactory::makeConnection();
+            $stmt_serie = $db->prepare($sqlSerie);
+            $stmt_serie->execute();
 
             }catch (PDOException $e){
                 echo $e->getMessage();
             }
-
+            return $stmt_serie;
         }
+
+        public function requestEpisode(int $row_serie):\PDOStatement
+        {
+            $stmt_serie = null;
+            try {
+
+                $sqlLstEps = "SELECT titre, file FROM episode where serie_id = ?";
+
+                $db = ConnectionFactory::makeConnection();
+                $stmt_serie = $db->prepare($sqlLstEps);
+                $stmt_serie->bindParam(1,$row_serie);
+                $stmt_serie->execute();
+
+            }catch (PDOException $e){
+                echo $e->getMessage();
+            }
+            return $stmt_serie;
+        }
+
+    /**
+     * @param int $id
+     * @param mixed $row_serie
+     * @param string $html
+     * @return string
+     */
+    private function getHtml(mixed $row_serie, string $html,int $id=-1): string
+    {
+        if (!$id == -1) {
+            $stmt = $this->requestEpisode($id);
+        }else {
+            $stmt = $this->requestEpisode($row_serie['id']);
+        }
+        $listeEpisode = [];
+        while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
+            $listeEpisode [] = new Episode($row['titre'], $row['file']);
+        }
+        $serie = new Serie($row_serie['titre'], $listeEpisode);
+        $renderer = new SerieRenderer($serie);
+
+        $user = unserialize($_SESSION["user_connected"]);
+        $pref = $user->pref;
+        $favoris = "";
+        if ($pref->isPref($row_serie["id"])) {
+            $favoris = "&favoris=2";
+        }
+        $html = "             
+            <a href='?action=display-serie&id={$row_serie['id']}$favoris'>
+            {$renderer->render(Renderer::COMPACT)} 
+            </a> <br>
+            ";
         return $html;
     }
 }
