814a9fee4608aaaed4f6c36cf11a31331ca8e895
Genamaoff <genamaoff@gmail.com>
2022-11-10 16:45:57 +0100

    getVisio

--- src/classes/application/User.php
+++ src/classes/application/User.php
@@ -78,6 +78,13 @@ class User
 
 
 
+    public function getVisio(): ClassVisio
+    {
+        return new CLassVisio(this->id);
+    }
+
+
+
     /**
      * @param string $email
      * @return string
