3f2ade6e90233b9d84f5dfad8c7f30f213217af3
Raphaël MANGIN <raphael.mangin6@etu.univ-lorraine.fr>
2022-11-10 10:08:46 +0100

    Amélioration modification email

--- src/classes/action/ModifyEmailAction.php
+++ src/classes/action/ModifyEmailAction.php
@@ -2,7 +2,9 @@
 
 namespace iutnc\netvod\action;
 
+use iutnc\netvod\auth\Auth;
 use iutnc\netvod\db\ConnectionFactory;
+use iutnc\netvod\exceptions\AuthException;
 
 class ModifyEmailAction extends Action
 {
@@ -24,16 +26,27 @@ class ModifyEmailAction extends Action
                             <input type="email" name="confirm-email">
                             <br /><br />
                             
+                            <label for="passwd">Entrez votre mot de passe</label>
+                            <input type="password" name="passwd">
+                            <br /><br />
+                            
                             <button type="submit">Valider</button>
                         </form> 
                         END;
                 } elseif ($this->http_method === 'POST') {
+                    $passwd = filter_var($_POST['passwd'], FILTER_SANITIZE_STRING);
                     $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
                     $confirm_email = filter_var($_POST['confirm-email'], FILTER_SANITIZE_EMAIL);
-                    if ($email !== $confirm_email) {
-                        $html = 'Les adresses emails sont différentes';
-                    } else {
-                        $html = $user->modifierEmail($email);
+                    try {
+                        Auth::authenticate($user->email, $passwd);
+                        if ($email !== $confirm_email) {
+                            $html = 'Les adresses emails sont différentes';
+                        } else {
+                            $html = $user->modifierEmail($email);
+                        }
+                    }catch (AuthException $e)
+                    {
+                        $html = $e->getMessage();
                     }
                 }
             }
