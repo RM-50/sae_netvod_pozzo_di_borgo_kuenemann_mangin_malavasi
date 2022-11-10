272a1788e9a52adc6772f47fa21a1933be644eb6
Genamaoff <genamaoff@gmail.com>
2022-11-10 16:24:53 +0100

    Update DisplaySerieAction.php

--- src/classes/action/DisplaySerieAction.php
+++ src/classes/action/DisplaySerieAction.php
@@ -51,50 +51,69 @@ class DisplaySerieAction extends Action
                     $visio = $user->visio;
                     $html .= "<br>";
 
+                    $favoris = "";
+                    $Visionnage="";
+
                     // Ajout/Suppression des favoris
                     if (filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT)
                         || filter_var($_GET['favoris'],FILTER_SANITIZE_STRING)) {
                         if (isset($_GET['favoris'])) {
                             if ($_GET['favoris'] == 3) {
+                                $favoris = "favoris=3";
                                 if ($pref->isPref($row_serie["id"])) {
                                     $pref->delPreference($_GET['id']);
                                 }
-                                $html .= "<br><a href='?action=display-serie&id={$_GET['id']}&favoris=1'>Ajoutez aux favoris</a>";
+                                $html .= "<br><a href='?action=display-serie&id={$_GET['id']}&favoris=1&$Visionnage'>Ajoutez aux favoris</a>";
                             } else if ($_GET['favoris'] == 1) {
+                                $favoris="favoris=1";
                                 if (!$pref->isPref($row_serie["id"])) {
                                     $mail = $user->email;
                                     $idUser = Serie::getIdUser($mail);
                                     $pref->addPreference($idUser, $_GET['id'], $serie);
                                 }
-                                $html .= "<a href='?action=display-serie&id={$_GET['id']}&favoris=3'>Retirez des favoris</a>";
+                                $html .= "<a href='?action=display-serie&id={$_GET['id']}&favoris=3&$Visionnage'>Retirez des favoris</a>";
                             } else if ($_GET['favoris'] == 2) {
-                                $html .= "<a href='?action=display-serie&id={$_GET['id']}&favoris=3'>Retirez des favoris</a>";
+                                $favoris="favoris=2";
+                                $html .= "<a href='?action=display-serie&id={$_GET['id']}&favoris=3&$Visionnage'>Retirez des favoris</a>";
                             }
-                        }else if (isset($_GET['EnCours'])) {
+                        }else {
+                            $html .= "<a href='?action=display-serie&id={$_GET['id']}&favoris=1&$Visionnage'>Ajoutez aux favoris</a>";
+                        }
+                        $html .= "<br>";
+                        $html .= "<br>";
+                        //Ajout / Suppression des vidéos en cours de lecture
+                        if (isset($_GET['EnCours'])) {
                             if ($_GET['EnCours'] == 3) {
-                                if (!$visio->isVisio($row_serie["id"])) {
-                                    $visio->delVisio($_GET['id']);
+                                $Visionnage="Visionnage=3";
+                                if (!$visio->isVideoEnCours($row_serie["id"])) {
+                                    $visio->delVideoEnCours($_GET['id']);
                                 }
-                                $html .= "<br><a href='?action=display-serie&id={$_GET['id']}&VIsionnage=1'>Ajoutez aux Visionnages</a>";
-                            } else if ($_GET['EnCours'] == 1) {
-                                if (!$visio->isVisio($row_serie["id"])) {
+                                $html .= "<br><a href='?action=display-serie&id={$_GET['id']}&$favoris&VIsionnage=1'>Ajoutez aux Visionnages</a>";
+                            }
+
+                            else if ($_GET['EnCours'] == 1) {
+                                $Visionnage="Visionnage=1";
+                                if (!$visio->isVideoEnCours($row_serie["id"])) {
                                     $mail = $user->email;
                                     $idUser = Serie::getIdUser($mail);
-                                    $visio->addVisio($idUser, $_GET['id'], $serie);
+                                    $visio->addVideoEnCours($idUser, $_GET['id'], $serie);
                                 }
-                                $html .= "<a href='?action=display-serie&id={$_GET['id']}&Visionnages=3'>Retirez des Visionnages</a>";
+                                $html .= "<a href='?action=display-serie&id={$_GET['id']}&$favoris&Visionnages=3'>Retirez des Visionnages</a>";
                             } else if ($_GET['EnCours'] == 2) {
-                                $html .= "<a href='?action=display-serie&id={$_GET['id']}&Visionnages=3'>Retirez des Visionnages</a>";
+                                $Visionnage="Visionnage=2";
+                                $html .= "<a href='?action=display-serie&id={$_GET['id']}&$favoris&Visionnages=3'>Retirez des Visionnages</a>";
                             }
 
                         }else {
-                            $html .= "<a href='?action=display-serie&id={$_GET['id']}&favoris=1'>Ajoutez aux favoris</a>";
-                            $html .= "<br>";
-                            $html .= "<br>";
-                            $html .= "<a href='?action=display-serie&id={$_GET['id']}&Visionnages=1'>Ajoutez aux Visionnages</a>";
+                            $html .= "<a href='?action=display-serie&id={$_GET['id']}&$favoris&Visionnages=1'>Ajoutez aux Visionnages</a>";
                         }
 
-                    }else {
+                    }
+
+
+
+
+                    else {
                         $html = "<h3>Vous ne pouvez pas modifier directement l'id de la serie</h3>";
                     }
 
@@ -113,11 +132,18 @@ class DisplaySerieAction extends Action
         {
             $html = "Vous devez vous connecter pour accéder à cette fonctionnalité";
         }
-        $id= $_GET['id'];
+
+
+
+        /*$id= $_GET['id'];
         $html .=<<<END
         </br>
+        <br>
+        <br>
         <button onclick="window.location.href='index.php?action=note&id=$id'">noter</button>
         END;
+        */
+
         return $html;
     }
 }
